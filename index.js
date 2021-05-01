d2b358964793008255a607d1ec9becfcbe582057
mountaindew2141@outlook.com <mountaindew2141@outlook.com>
2021-05-01 12:42:58 -0400

    added prompt function for main menu

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "~Ma009090",
+  password: "password",
   database: "employeesDB"
 });
 
@@ -18,3 +18,60 @@ connection.connect(function (err) {
   firstPrompt();
 });
 
+// function which prompts the user for what action they should take
+function firstPrompt() {
+
+  inquirer
+    .prompt({
+      type: "list",
+      name: "task",
+      message: "Would you like to do?",
+      choices: [
+        "View Employees",
+        "View Employees by Department",
+        // "View Employees by Manager",
+        "Add Employee",
+        "Remove Employees",
+        "Update Employee Role",
+        "Add Role",
+        // "Remove Role",
+        // "Update Employee Manager",
+        "End"]
+    })
+    .then(function ({ task }) {
+      switch (task) {
+        case "View Employees":
+          viewEmployee();
+          break;
+        case "View Employees by Department":
+          viewEmployeeByDepartment();
+          break;
+        // case "View Employees by Manager":
+        //   viewEmployeeByManager();
+        //   break;
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "Remove Employees":
+          removeEmployees();
+          break;
+        case "Update Employee Role":
+          updateEmployeeRole();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+        // case "Remove Role":
+        //   removeRole();
+        //   break;
+
+        // case "Update Employee MAnager":
+        //   updateEmployeeManager();
+        //   break;
+
+        case "End":
+          connection.end();
+          break;
+      }
+    });
+}
